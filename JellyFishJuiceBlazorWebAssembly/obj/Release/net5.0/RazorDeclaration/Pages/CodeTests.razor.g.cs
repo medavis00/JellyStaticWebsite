a7c33dc058f898c5ed6c4fe332a3df738c891638
Mark Davis <medavis00@live.com>
2022-01-15 15:39:10 -0800

    add test api

--- JellyFishJuiceBlazorWebAssembly/obj/Release/net5.0/RazorDeclaration/Pages/CodeTests.razor.g.cs
+++ JellyFishJuiceBlazorWebAssembly/obj/Release/net5.0/RazorDeclaration/Pages/CodeTests.razor.g.cs
@@ -10,92 +10,106 @@ namespace JellyFishJuiceBlazorWebAssembly.Pages
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
-using System.Net.Http;
-
-#line default
-#line hidden
-#nullable disable
-#nullable restore
-#line 2 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
-using System.Net.Http.Json;
-
-#line default
-#line hidden
-#nullable disable
-#nullable restore
-#line 3 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 3 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 4 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 5 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 6 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 7 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 8 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 9 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using JellyFishJuiceBlazorWebAssembly;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 10 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using JellyFishJuiceBlazorWebAssembly.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 11 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using JellyStaticBlazor.Common;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 12 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using JellyFishJuiceBlazorWebAssembly.Services;
 
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 3 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\Pages\CodeTests.razor"
+using System.Net.Http;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 4 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\Pages\CodeTests.razor"
+using System.Net.Http.Json;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 5 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\Pages\CodeTests.razor"
+using System.Threading.Tasks;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 6 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\Pages\CodeTests.razor"
+using JellyFishJuiceBlazorWebAssembly.Controllers;
+
+#line default
+#line hidden
+#nullable disable
+    [Microsoft.AspNetCore.Components.RouteAttribute("/codetests")]
     public partial class CodeTests : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
@@ -103,6 +117,22 @@ using JellyFishJuiceBlazorWebAssembly.Services;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 28 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\Pages\CodeTests.razor"
+       
+
+    public List<TestDTO> codecamps = new List<TestDTO>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        codecamps = await TestService.GetTests();
+    }
+
+#line default
+#line hidden
+#nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private ITestService TestService { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private HttpClient Http { get; set; }
     }
 }
 #pragma warning restore 1591
--- JellyFishJuiceBlazorWebAssembly/obj/Release/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ JellyFishJuiceBlazorWebAssembly/obj/Release/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,84 +13,84 @@ namespace JellyFishJuiceBlazorWebAssembly.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 1 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 2 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 3 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 4 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 5 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 6 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 7 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 8 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 9 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using JellyFishJuiceBlazorWebAssembly;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 10 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using JellyFishJuiceBlazorWebAssembly.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 11 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using JellyStaticBlazor.Common;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
+#line 12 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\_Imports.razor"
 using JellyFishJuiceBlazorWebAssembly.Services;
 
 #line default
@@ -104,7 +104,7 @@ using JellyFishJuiceBlazorWebAssembly.Services;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 43 "C:\Users\medav\source\repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\Shared\NavMenu.razor"
+#line 51 "C:\Users\medav\Source\Repos\JellyStaticWebsite\JellyFishJuiceBlazorWebAssembly\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
